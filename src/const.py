8ad33cfdd84433a325bcd25e36bde721b198d136
So <so.x.9137@gmail.com>
2025-07-03 00:21:29 +0900

    feat: support Japanese file (#2)

---
+++ src/const.py
@@ -0,0 +1,26 @@
+rules_dict = {
+    "en": ["Splat Zones", "Tower Control", "Rainmaker", "Clam Blitz"],
+    "ja": ["ガチエリア", "ガチヤグラ", "ガチホコ", "ガチアサリ"]
+}
+
+x_power_patterns_dict = {
+    "en": r"X Power[:\s]+([\d\.]+)",
+    "ja": r"Xパワー[:\s]+([\d\.]+)"
+}
+
+keywords_dict = {
+    "en": {
+        "victory": "victory",
+        "defeat": "defeat",
+        "score": "score",
+        "score_prefix": "Score: ",
+        "knockout": "knockout",
+    },
+    "ja": {
+        "victory": "win",
+        "defeat": "lose",
+        "score": "カウント",
+        "score_prefix": "",
+        "knockout": "ノックアウト！"
+    }
+}
